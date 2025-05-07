51204ac58728f3c62f00b1d8be4719c828e68d44
JonatasFelipe01 <jonatas.bazinga@gmail.com>
2025-05-07 18:44:50 +0000

    Atividade1_Feita

--- Jonatas/Atividade1.py
+++ Jonatas/Atividade1.py
@@ -1,2 +1,14 @@
 # Observe o quadro de atividades 
 # Crie 5 variaveis aluno , idade , salario , escola , quantidadeAlunos . -> Exiba qualquer coisa que de preferencia exiba na tela esses resultados
+
+aluno = "Jônatas Felipe"
+idade = 19
+salario = 300.00
+escola = "Senai"
+quantidadeAlunos = 1
+
+print(aluno)
+print(idade)
+print(salario)
+print(escola)
+print(quantidadeAlunos)
