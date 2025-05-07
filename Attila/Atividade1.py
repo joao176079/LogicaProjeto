2ca797ea8757276171302dc207243b55249599cb
CatMan <65993223+Attila31312@users.noreply.github.com>
2025-05-07 17:42:56 +0000

    atividade1_feita

--- Attila/Atividade1.py
+++ Attila/Atividade1.py
@@ -1,2 +1,16 @@
 # Observe o quadro de atividades 
 # Crie 5 variaveis aluno , idade , salario , escola , quantidadeAlunos . -> Exiba qualquer coisa que de preferencia exiba na tela esses resultados
+import os
+os.system ("cls || clear ")
+
+aluno = str(input("Informe o nome do aluno: "))
+idade = int (input("Informe a idade: "))
+salario = float (input("Informe o salario: "))
+escola = str (input("Informe o nome da escola: "))
+quantidadeAlunos = int (input("Informe a quantidade de alunos: "))
+
+print (f"Nome do aluno: {aluno}")
+print (f"Idade: {idade}")
+print (f"Salario: {salario}")
+print (f"Nome da escola: {escola}")
+print (f"Quantidade de alunos: {quantidadeAlunos}")
--- Luiz/Atividade1.py
+++ Luiz/Atividade1.py
@@ -1,5 +1,7 @@
 # Observe o quadro de atividades 
 # Crie 5 variaveis aluno , idade , salario , escola , quantidadeAlunos . -> Exiba qualquer coisa que de preferencia exiba na tela esses resultados
+import os
+os.system('clear')
 
 aluno1 = "Luiz"
 aluno2 = "Maria"
